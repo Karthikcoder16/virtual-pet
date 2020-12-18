9fa5cc92d3e7a90d308394f7bce2095f890e0879
Karthikcoder16 <71510293+Karthikcoder16@users.noreply.github.com>
2020-12-18 17:42:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+//Create variables here
+ var pet;
+function preload()
+{ this.Image=loadImage("sprites/doglmg.png");
+	//load images here
+}
+
+function setup() {
+  createCanvas(800, 700);
+  
+  var pet=createSprite(200,200,20,20);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
